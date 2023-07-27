cc7622d56517af8a97bfc94f1073299e5cddc238
Andrew Sanchez <druu831@gmail.com>
2023-07-27 10:55:12 -0700

    db pagination set up

--- controllers/products.js
+++ controllers/products.js
@@ -40,6 +40,13 @@ const getAllProducts = async (req, res) => {
     result.select(fieldsList);
   }
 
+  // Defaulting to page 1 and limit of 10
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, hits: products.length });
 };
